a63ce910c97b89cec3c543963a4cca95773a0ebc
abel mengich <kipyegoabel2308@gmail.com>
2024-03-20 20:58:24 -0400

    test passed

---
+++ index.js
@@ -0,0 +1,21 @@
+// Function to divide two numbers
+const divide = (a, b) => {
+    return a / b;
+  };
+  
+  // Arrow function to square a number
+  const square = (x) => {
+    return x * x;
+  };
+  
+  // Arrow function to add two numbers
+  const add = (a, b) => {
+    return a + b;
+  };
+  
+  module.exports = {
+    divide,
+    square,
+    add
+  };
+  
